023baf43fb7d4f96e3c910f5526a47ca7af34efd
Santhu2022 <mogilipakasantosh@gmail.com>
2023-05-05 14:25:22 +0000

    Full Code

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -4,9 +4,9 @@ const PlanetItem = props => {
   const {planetDetails} = props
   const {name, imageUrl, description} = planetDetails
   return (
-    <div className="planet-item">
-      <img src={imageUrl} alt={`planet ${name}`} className="planet-image" />
-      <h1 className="planet-name">{name}</h1>
+    <div className="planet-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="image" />
+      <h1 className="name">{name}</h1>
       <p className="description">{description}</p>
     </div>
   )
--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -4,21 +4,15 @@ import PlanetItem from '../PlanetItem'
 import './index.css'
 
 const PlanetsSlider = props => {
-  const settings = {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }
   const {planetsList} = props
   return (
     <div className="planets-container" data-testid="planets">
       <h1 className="app-heading">Planets</h1>
-      <div className="slider-container">
-        <Slider {...settings}>
-          {planetsList.map(eachPlanet => (
-            <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
-          ))}
-        </Slider>
-      </div>
+      <Slider>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
     </div>
   )
 }
